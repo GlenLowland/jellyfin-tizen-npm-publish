b42a05a7abae154ea1bb72f9f3a9aac21c3d39f4
GlenLowland <127740393+GlenLowland@users.noreply.github.com>
2024-04-04 14:16:05 +0000

    Create tizen.js

---
+++ tizen.js
@@ -0,0 +1,104 @@
+"use strict";
+
+(function () {
+    let client;
+    let systemInfo = {};
+    let productInfo;
+
+    const currentApplication = {
+        appInfo: {
+            // Just emulate the value cause I don't see any sense to provide TizenBrew version here
+            version: 'DEVELOPMENT'
+        }
+    };
+
+    window.tizen = {
+        application: {
+            getCurrentApplication: () => currentApplication
+        },
+        systeminfo: {
+            getPropertyValue: (propertyValue, cb) => {
+                if (systemInfo[propertyValue]) {
+                    return cb(systemInfo[propertyValue]);
+                }
+                send({ type: 'requestSystemInfo', propertyValue });
+
+                let tries = 0;
+                const interval = setInterval(() => {
+                    if (systemInfo[propertyValue]) {
+                        return cb(systemInfo[propertyValue]);
+                    }
+                    tries++;
+                    if (tries === 10) {
+                        clearInterval(interval);
+                        alert('It took too long to recieve Tizen system info!');
+                        return cb({});
+                    }
+                }, 100);
+            }
+        },
+        tvinputdevice: {
+            // a placeholder, keys are registered in a parent app anyway
+            registerKey: () => {},
+            // a placeholder for now
+            unregisterKey: () => {}
+        }
+    };
+
+    window.webapis = {
+        productinfo: {
+            is8KPanelSupported: () => productInfo.is8KPanelSupported,
+            isUdPanelSupported: () => productInfo.isUdPanelSupported
+        }
+    };
+
+    function connect() {
+        const ip = localStorage.getItem('ip');
+        try {
+            if (ip.includes(':')) {
+                client = new WebSocket(`ws://${ip}`);
+            } else {
+                client = new WebSocket(`ws://127.0.0.1:8081`);
+            }
+            client.onmessage = onMessage;
+            client.onopen = onOpen;
+            client.onerror = () => {
+                localStorage.removeItem('ip');
+                window.location.reload();
+            }
+        } catch(e) {
+            localStorage.removeItem('ip');
+            window.location.reload();
+        }
+    }
+
+    window.send = (message) => {
+        client.send(JSON.stringify(message));
+    }
+
+    function onMessage(msg) {
+        const message = JSON.parse(msg.data);
+        switch (message.type) {
+            case 'systemInfo': {
+                systemInfo[message.propertyValue] = message.result;
+                break;
+            }
+
+            case 'productInfo': {
+                productInfo = message.result;
+                break;
+            }
+
+            default: {
+                // ignore
+            }
+        }
+    }
+
+    function onOpen() {
+        // We have to get the product info cause it has no async api.
+        send({ type: 'requestProductInfo' });
+    }
+
+    connect();
+})();
