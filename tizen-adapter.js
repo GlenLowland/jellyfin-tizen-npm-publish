68ef8e04fcfa7b8763dabb26d88e74abe5cc12c9
GlenLowland <127740393+GlenLowland@users.noreply.github.com>
2024-10-11 21:44:31 +0100

    Remove tizen API rebinding
    No longer needed with the new TizenBrew

--- tizen-adapter.js
+++ tizen-adapter.js
@@ -1,40 +1,6 @@
 "use strict";
 
 (function () {
-    const currentApplication = {
-        appInfo: {
-            // Just emulate the value cause I don't see any sense to provide TizenBrew version here
-            version: 'DEVELOPMENT'
-        },
-        exit: () => {
-            // cannot do anything about it :(
-        }
-    };
-
-    window.tizen = {
-        application: {
-            getCurrentApplication: () => currentApplication
-        },
-        systeminfo: {
-            getPropertyValue: (propertyValue, cb) => {
-                if (propertyValue === 'DISPLAY') {
-                    return {
-                        resolutionWidth: window.screen.width,
-                        resolutionHeight: window.screen.height
-                    };
-                } else {
-                    return {};
-                }
-            }
-        },
-        tvinputdevice: {
-            // a placeholder, keys are registered in a parent app anyway
-            registerKey: () => {},
-            // a placeholder for now
-            unregisterKey: () => {}
-        }
-    };
-
     window.webapis = {
         productinfo: {
             is8KPanelSupported: () => false,
